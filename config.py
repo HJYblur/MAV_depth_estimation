df739352801dd79c3b4b6aee6fb185b067ed675c
pietbcm <pietbouwens.cm@gmail.com>
2025-03-18 19:03:44 +0100

    Add accurate camera FOV in eval

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ config = {
     'image_height': 520,
     "image_mode": "YUV", # "RGB" or "L" (grayscale) or "UYVY" or "YUV"
     "input_type_uint8": False,
-    "cam_hor_FOV": 100,
+    "cam_hor_FOV": 115, # In degrees. Yes, I measured :)
     
     # Model configurations
     "input_channels": 3, # 3 for RGB, 1 for grayscale, 1 for uyuv(1*H*2W), 3 for yuv
